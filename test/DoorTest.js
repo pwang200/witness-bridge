2df17541ab7e1a420f7cb7a0690aba1c09c60302
Peng Wang <pwang200@gmail.com>
2022-08-04 15:56:57 -0400

    fixed unittests and cleanup

--- test/DoorTest.js
+++ test/DoorTest.js
@@ -1,9 +1,7 @@
-//const { inputToConfig } = require("@ethereum-waffle/compiler");
 const { assert, expect } = require("chai");
 const { ethers } = require("hardhat")
 
 describe("Door contract tests", function () {
-    //const { log } = deployments
     quorum = 2
     transferMin = 1_234_000_000n
     accountCreateMin = 2_000_000_000n
@@ -12,19 +10,17 @@ describe("Door contract tests", function () {
 
     let doorFactory, door1, door2, witness1, witness2, Alice, Bob
     beforeEach(async function () {
-        [deployer1, deployer2, witness1, witness2, Alice, Bob] = await ethers.getSigners()
+        [deployer, witness1, witness2, Alice, Bob] = await ethers.getSigners()
         witnesses = [witness1.address, witness2.address]
 
-        //console.log(`deploying two doors from ${deployer1.address} and ${deployer2.address} ...`)
-        doorFactory = await ethers.getContractFactory("Door", deployer1)
+        doorFactory = await ethers.getContractFactory("Door", deployer)
         door1 = await doorFactory.deploy(quorum, transferMin, accountCreateMin, sigRewardMin, witnesses, { value: initReserve });
         await door1.deployed()
 
-        doorFactory = doorFactory.connect(deployer2)
         door2 = await doorFactory.deploy(quorum, transferMin, accountCreateMin, sigRewardMin, witnesses, { value: initReserve });
         await door2.deployed()
         //console.log(`deployed two doors at ${door1.address} and ${door2.address}`)
-        //console.log(`Alice and Bob addresses ${Alice.address} ${Bob.address}`)
+        //console.log(`deployer ${deployer.address} Alice ${Alice.address} Bob ${Bob.address}`)
     })
     // describe("simpler", function () { }) // nest is ok
 
@@ -41,33 +37,31 @@ describe("Door contract tests", function () {
     })
 
     it("claimId event", async function () {
-        //const BobAddress = "BobAddress" //TODO Bob.address.toString() won't work
-        const BobAddress = Bob.address
+        const BobAddress = Bob.address.toString().toLowerCase()
         await expect(door1.XChainClaimIdCreate(BobAddress))
             .to.emit(door1, "XChainClaimIdEvent")
             .withArgs(BobAddress, 1)
-
-        // const tx = await door1.XChainClaimIdCreate("BobAddress")
-        // const receipt = await tx.wait()
-        // for (const event of receipt.events) {
-        //     console.log(`Event ${event.event} with args ${event.args}`);
-        // }
+        await expect(door1.XChainClaimIdCreate(BobAddress))
+            .to.emit(door1, "XChainClaimIdEvent")
+            .withArgs(BobAddress, 2)
     })
 
     it("commit events", async function () {
-        const BobAddress = Bob.address
+        const BobAddress = Bob.address.toString().toLowerCase()
+        const AliceAddress = Alice.address.toString()
+
         // enum XChainCommitType {
         //     createAccount,
         //     transferAsset
         // }
         // so createAccount's value is 0, and transferAsset's value is 1
-        await expect(door2.XChainCommit(1, sigRewardMin, BobAddress, { value: transferMin }))
+        await expect(door2.connect(Alice).XChainCommit(1, sigRewardMin, BobAddress, { value: transferMin }))
             .to.emit(door2, "XChainCommitEvent")
-            .withArgs(1, 1, transferMin, sigRewardMin, BobAddress);
+            .withArgs(1, 1, transferMin, sigRewardMin, AliceAddress, BobAddress);
 
-        await expect(door2.XChainCreateAccountCommit(sigRewardMin, BobAddress, { value: accountCreateMin }))
+        await expect(door2.connect(Alice).XChainCreateAccountCommit(sigRewardMin, BobAddress, { value: accountCreateMin }))
             .to.emit(door2, "XChainCommitEvent")
-            .withArgs(0, 1, accountCreateMin, sigRewardMin, BobAddress);
+            .withArgs(0, 1, accountCreateMin, sigRewardMin, AliceAddress, BobAddress);
     })
 
     it("transfer", async function () {
